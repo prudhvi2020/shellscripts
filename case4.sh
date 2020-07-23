8a0bce81a46e0816e4d8acc1bc0bb0d28c4489d6
Ubuntu <ubuntu@ip-172-31-34-131.ec2.internal>
2020-07-23 11:50:17 +0000

    script on files

---
+++ case4.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+echo -e "\e[33m A - DISPLAY THE CONTENT\e[0m"
+echo -e "\e[32m-------------------------------\e[0m"
+echo -e "\e[36m B - APPEND THE CONTENT\e[0m"
+echo -e "\e[32m-------------------------------\e[0m"
+echo -e "\e[31m C - OVERWRITE THE CONTENT\e[0m"
+echo -e "\e[32m-------------------------------\e[0m"
+echo -e "\e[32m D - DELETE THE CONTENT\e[0m"
+echo -e "\e[32m-------------------------------\e[0m"
+echo -e "\e[35m E - REMOVE THE FILE\e[0m"
+echo -e "\e[32m-------------------------------\e[0m"
+read -p "chose your option A|B|C|D|E:" option
+echo -e "\e[32m-------------------------------\e[0m"
+read -p "Enter the file name u want to Perform the operation:" fname
+
+case $option in
+  A )  
+     if [ ! -s $fname ] ; then
+	 echo "it is an empty file"
+     else
+	echo "the content of the file is "
+	echo "--------------------------"
+	cat $fname
+     fi
+     ;;
+  B )
+      read -p "Enter the data which u want:" data
+      echo $data >> $fname
+      echo "data apended"
+      ;;
+  C )
+      read -p "Enter the data which u want to overwrite:" data
+      echo $data > $fname
+      echo "old data should be overwrite"
+      ;;
+  D )
+      read -p "enter the file which u want to delete:" data
+      cat /dev/null > $fname
+      echo "content of the file is deleted"
+      ;;
+  E )
+      read -p "Enter the file which u want to delete:" fname
+      rm -r $fname
+      echo "$fname file is deleted successfully"
+      ;;
+       
+   * )
+	 echo "plz enter correct file"
+
+esac
